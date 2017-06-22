7f6f29367accc0aea8c136fa6b4bb8494f15c4dd
Remco Visser <rvremco@hotmail.com>
2017-06-22 11:38:42 +0200

    Added cool stuff

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,6 @@
 var api_url = "http://project7-8-webinterface.dev/locations";
 var markers = [];
+var circles = [];
 var map;
 var updateMarkersRate = 10000;
 
@@ -10,19 +11,37 @@ $(function() {
         // Clear markers
         for (var i = 0; i < markers.length; i++ ) {
             markers[i].setMap(null);
+            circles[i].setMap(null);
         }
-        markers.length = 0;
         // Add markers
         $.get( api_url, function( data ) {
             for(var i = 0; i < data.length; i++) {
                 var location = data[i];
 
+                // Create Icon object for a marker
+                var icon = {
+                    url: "http://icons.iconarchive.com/icons/iconsmind/outline/512/Shopping-Cart-icon.png", // url
+                    scaledSize: new google.maps.Size(50, 50), // scaled size
+                    anchor: new google.maps.Point(25, 25) // anchor
+                };
+                // Create a marker at the location of a cart
                 var marker = new google.maps.Marker({
-                    label: location.cart_id.toString(),
+                    icon: icon,
                     position: new google.maps.LatLng(location.latitude, location.longitude),
                     map: map
                 });
                 markers.push(marker);
+                // Create a circle around a cart to show its accuracy
+                var circle = new google.maps.Circle({
+                    center: markers[0].position,
+                    radius: 100,
+                    map: map,
+                    fillColor: '#24a337',
+                    fillOpacity: 0.1,
+                    strokeColor: '#ffffff',
+                    strokeOpacity: 0.3
+                });
+                circles.push(circle);
             }
         });
         timerLoop = setTimeout(timer, updateMarkersRate);
@@ -50,4 +69,5 @@ function initMap() {
 function blockClicked(block_id) {
     var marker = markers[block_id];
     map.setCenter(new google.maps.LatLng(marker.position.lat(), marker.position.lng()));
+    map.setZoom(17);
 }
